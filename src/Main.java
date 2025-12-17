dfdafd44dbc981946c3a40da3bf945540924b7bf
user <orozalitoktobaev12@gmail.com>
2025-12-17 12:34:27 +0600

    Covbn

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,258 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-void main() {
-  //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-  // to see how IntelliJ IDEA suggests fixing it.
-  IO.println(String.format("Hello and welcome!"));
+import javax.xml.catalog.Catalog;
+import java.io.PrintWriter;
+import java.lang.reflect.GenericArrayType;
+import java.security.cert.CertPathValidatorException;
+import java.util.*;
+import java.util.Scanner;
+import java.util.logging.SocketHandler;
 
-  for (int i = 1; i <= 5; i++) {
-    //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-    // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-    IO.println("i = " + i);
-  }
-}
+public class Main {
+    public static void main(String[] arg) {
+
+
+//        System.out.println(Math.sqrt(10));
+//        // 2*2*2
+//        System.out.println(Math.pow(2,3));
+//        //Баашын эле берет
+//        System.out.println(Math.round(3.4));
+//        // Паталок
+//        System.out.println(Math.ceil(4.1));
+//        // ылдый
+//        System.out.println(Math.floor(5.5));
+
+
+
+//     Срок сдачи: ----
+// Треугольник деген класс тузунуз.
+//Класстын 3 поляси болсун a, b, c.
+//Класстын "площадь" деген методу болсун.
+//метод "площадь" полелериндеги маалыматтарга(маалымат объектти тузгондон кийин берилет)
+//таянып консолго уч бурчтуктун аянтын чыгарсын.
+
+        Scanner sc = new Scanner(System.in);
+        Triangle triangle = new Triangle();
+        triangle.a=sc.nextInt();
+        triangle.b=sc.nextInt();
+        triangle.c=sc.nextInt();
+        triangle.square();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        }
+
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ src/Triangle.java
@@ -0,0 +1,12 @@
+public class Triangle {
+    double a;
+    double b;
+    double c;
+
+ public void square(){
+        System.out.println(a*b*c/2);
+
+
+    }
+
+}
